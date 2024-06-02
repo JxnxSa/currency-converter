22d952678ee52e67db7398ca6b7f910e9ada83c2
JxnxSa <kwang41jane@gmail.com>
2024-06-02 19:23:44 +0700

    edit service

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -16,13 +16,6 @@ export class ServiceService {
         AFN: 19.54902,
         ALL: 25.3909,
         AMD: 105.53568,
-        ANG: 0.48646,
-        AOA: 232.34593,
-        ARS: 243.55344,
-        AUD: 0.4098,
-        AWG: 0.49263,
-        AZN: 0.4625,
-        BAM: 0.49085,
       },
     },
     AFN: {
@@ -31,13 +24,6 @@ export class ServiceService {
         AFN: 1,
         ALL: 1.2993,
         AMD: 5.4015,
-        ANG: 0.02488,
-        AOA: 11.88157,
-        ARS: 12.45673,
-        AUD: 0.02096,
-        AWG: 0.0252,
-        AZN: 0.02366,
-        BAM: 0.02511,
       },
     },
     ALL: {
@@ -46,13 +32,6 @@ export class ServiceService {
         AFN: 0.7696,
         ALL: 1,
         AMD: 4.21,
-        ANG: 0.01915,
-        AOA: 9.1497,
-        ARS: 9.5958,
-        AUD: 0.01615,
-        AWG: 0.0194,
-        AZN: 0.01834,
-        BAM: 0.01945,
       },
     },
     AMD: {
@@ -61,13 +40,6 @@ export class ServiceService {
         AFN: 0.1845,
         ALL: 0.2375,
         AMD: 1,
-        ANG: 0.00455,
-        AOA: 2.1759,
-        ARS: 2.2823,
-        AUD: 0.00384,
-        AWG: 0.0046,
-        AZN: 0.00435,
-        BAM: 0.0046,
       },
     },
   };
@@ -84,7 +56,7 @@ export class ServiceService {
   getApiFetchAll(
     from: string
   ): Observable<{ results: { [key: string]: number } }> {
-    const url = `https://api.fastforex.io/fetch-alll?from=${from}&api_key=${this.api_key}`;
+    const url = `https://api.fastforex.io/fetch-all?from=${from}&api_key=${this.api_key}`;
     return this.http.get<{ results: { [key: string]: number } }>(url).pipe(
       map((response) => ({
         results: response.results,
