cdf8bd34a79c875bf4c0ceee9e7b77ca79f8c14f
JxnxSa <kwang41jane@gmail.com>
2024-06-03 21:53:59 +0700

    delete comment

--- src/app/exchange-page/exchange-page.component.ts
+++ src/app/exchange-page/exchange-page.component.ts
@@ -1,53 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { ServiceService } from '../service.service';
-// import { Rate } from '../models/rate';
-
-// @Component({
-//   selector: 'app-exchange-page',
-//   templateUrl: './exchange-page.component.html',
-//   styleUrls: ['./exchange-page.component.css'],
-// })
-// export class ExchangePageComponent implements OnInit {
-//   rates: Rate[] = [];
-//   selectedCurrency: string = '';
-
-//   page = 1;
-//   currencyList: string[] = [];
-//   amount: number = 0;
-//   convertedAmount: number | null = null;
-//   targetCurrency: string = '';
-
-//   constructor(private apiSer: ServiceService) {}
-
-//   getDataFetchAll() {
-//     this.apiSer.getApiFetchAll(this.selectedCurrency).subscribe((res) => {
-//       this.rates = this.convertData([res]);
-//       console.log(this.rates);
-//     });
-//   }
-
-//   convertData(data: any): Rate[] {
-//     let _data: Rate[] = [];
-//     _data.push(data[0].results || '');
-
-//     return _data;
-//   }
-
-//   getCurrencyList() {
-//     this.apiSer.getApiCurrencyList().subscribe(
-//       (res) => {
-//         this.currencyList = res;
-//       },
-//       (error) => {
-//         console.error('Error fetching currency list', error);
-//       }
-//     );
-//   }
-
-//   ngOnInit(): void {
-//     this.getCurrencyList();
-//   }
-// }
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../service.service';
 import { Rate } from '../models/rate';
@@ -65,7 +15,6 @@ export class ExchangePageComponent implements OnInit {
   currencyList: string[] = [];
   amount: number = 0;
   convertedAmount: number | null = null;
-  targetCurrency: string = '';
 
   constructor(private apiSer: ServiceService) {}
 
